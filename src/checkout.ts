cd0c7e556b3056f23a208b48844dc610b4a3ba58
benjamin GIRARD <benjamingirard25@gmail.com>
2023-07-07 16:06:58 +0200

    we can apply discount, now

--- src/checkout.ts
+++ src/checkout.ts
@@ -1,6 +1,15 @@
 import { itemFactory } from "./Item";
 
-export const checkout = (itemsList: string): number =>
-  itemsList
+export const checkout = (itemsList: string): number => {
+  const reduction = itemsList
     .split("")
     .reduce((acc, itemChar) => acc + itemFactory(itemChar).getPrice(), 0);
+
+  const A_counter = itemsList.split("").filter((item) => item === "A").length;
+  const B_counter = itemsList.split("").filter((item) => item === "B").length;
+
+  const total =
+    reduction - Math.trunc(A_counter / 3) * 20 - Math.trunc(B_counter / 3) * 15;
+
+  return total;
+};
--- test/checkout.test.ts
+++ test/checkout.test.ts
@@ -20,4 +20,17 @@ describe("checkout test", () => {
   it("Should throw an error when sending an invalid item", () => {
     expect(() => checkout("Z")).toThrow("Unknown item");
   });
+
+  it.each`
+    checkoutValue | result
+    ${"AAA"}      | ${130}
+    ${"AAAAAAAA"} | ${360}
+    ${"BBB"}      | ${75}
+    ${"BBBBBBBB"} | ${210}
+  `(
+    "Should apply discount when we having $checkoutValue",
+    ({ checkoutValue, result }) => {
+      expect(checkout(checkoutValue)).toBe(result);
+    }
+  );
 });
